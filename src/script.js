3f6c76059e2f6a1a4739e2f856034ee655a5f53a
DanielPincu <ghepin01@easv365.dk>
2023-11-28 11:04:00 +0100

    Buttons OK

--- src/script.js
+++ src/script.js
@@ -16,3 +16,30 @@ document.addEventListener("DOMContentLoaded", function() {
     var x = document.getElementById("showOrHide");
     x.style.display = "none";
 });
+
+
+
+
+
+
+
+
+
+function showOrHideDiv2() {
+    var x = document.getElementById("showOrHide2");
+    var btnText = document.getElementById("myBtn2");
+    
+    if (x.style.display === "none" || x.style.display === "") {
+        x.style.display = "block";
+        btnText.innerHTML = "Read less";
+    } else {
+        x.style.display = "none";
+        btnText.innerHTML = "Read more";
+    }
+}
+
+// Hide the div initially when the page loads
+document.addEventListener("DOMContentLoaded", function() {
+    var x = document.getElementById("showOrHide2");
+    x.style.display = "none";
+});
